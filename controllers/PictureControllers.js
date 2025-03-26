c73049b718b18aa881e9dbcd1bfd34b3f53ee389
JENIFFER DE ARAUJO STEINBACK DO NASCIMENTO <jeniffer.asnasciment@senacsp.edu.br>
2025-03-25 22:06:21 -0300

    Criação API

---
+++ controllers/PictureControllers.js
@@ -0,0 +1,36 @@
+const Picture = require("../models/Picture");
+
+const fs = require("fs");
+
+exports.create = async (req, res) => {
+    res.json("Ok!");
+
+    try {
+        const { name } = req.body;
+
+        const file = req.file;
+
+        const picture = new Picture ({
+            name,
+            src: file.path,
+        });
+
+        await picture.save ();
+
+        res.json({ picture, msg: "Imagem salva com sucesso!"});
+        
+    } cath (error) {
+        res.status(500).json({ message: "Erro ao salvar!"});
+        
+    }
+};
+
+exports.findAll = async (req, res) => {
+    try {
+        const pictures = await Picture.find();
+
+        res.json(pictures);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar!" });
+    }
+};
---
+++ models/Picture.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+
+const PictureSchema = new Schema ({
+    name: {type: String, require: true },
+    scr: {type: String, required: true},
+
+});
+
+module.exports = mongoose.model("Picture", PictureSchema);
